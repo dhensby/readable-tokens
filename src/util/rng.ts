11a4406c727e7953104e2db4f75e175a94ef6649
Dan Hensby <git@hens.by>
2023-06-29 10:23:52 +0100

    fix: catch sync errors from rng

--- src/util/rng.ts
+++ src/util/rng.ts
@@ -2,12 +2,16 @@ import { randomBytes } from 'crypto';
 
 export async function rng(length: number): Promise<Buffer> {
     return new Promise((resolve, reject) => {
-        randomBytes(length, (err, buf) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(buf);
-            }
-        });
+        try {
+            randomBytes(length, (err, buf) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(buf);
+                }
+            });
+        } catch (e) {
+            reject(e);
+        }
     });
 }
